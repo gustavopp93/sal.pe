8e1a6650d0d08bc0c060fd45f5df871b6e79add9
gustavopp93 <p1993.gustavo@gmail.com>
2015-10-11 23:09:32 -0500

    add admin for user and organization models

--- salPe/apps/organizations/admin.py
+++ salPe/apps/organizations/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.organizations.models import Organization
+
+
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'status', )
--- salPe/apps/profiles/admin.py
+++ salPe/apps/profiles/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.profiles.models.custom_user import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'is_active', )
+
